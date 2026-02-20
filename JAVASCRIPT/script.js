d577171ce3f946e7207705d1d185add33450f3b1
christianpm-hub <cpermon610@g.educaand.es>
2026-02-20 10:53:47 +0100

    Prueba3 modo noche terminada

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,5 @@
+function modonoche(){
+            const texto = document.getElementById("texto");
+            texto.style.color = "white";
+            document.body.style.backgroundColor = "black";
+        }
