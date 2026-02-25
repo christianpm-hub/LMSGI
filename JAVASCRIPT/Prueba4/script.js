8c18a34cc29bdfae2f0f974ea9ce80c0636664f8
christianpm-hub <cpermon610@g.educaand.es>
2026-02-25 14:35:17 +0100

    Prueba 4 con update

--- JAVASCRIPT/Prueba4/script.js
+++ JAVASCRIPT/Prueba4/script.js
@@ -1,13 +1,36 @@
 const button = document.getElementById('agregar');
-const tareas = document.getElementById('tareas');
+const listatareas = document.getElementById('listatareas');
 const entrada = document.getElementById('entrada');
+const bcambiarcolor = document.getElementById('cambiarcolor');
 
 button.addEventListener('click', agregar);
+bcambiarcolor.addEventListener('click', colorrojo);
+
+function colorrojo(){
+    const listatareasli = document.querySelectorAll('li');
+    for (let i = 0; i<listatareasli.length;i++){
+        listatareasli[i].style.color = 'red';
+    }
+}
 
 function agregar(){
-    const ptarea = document.createElement('p');
+    const litarea = document.createElement('li');
+
     const node = document.createTextNode(entrada.value);
-    ptarea.appendChild(node);
+    litarea.appendChild(node);
     
-    tareas.appendChild(ptarea);
+    listatareas.appendChild(litarea);
+
+    /*
+    const enlace = document.createElement('a');
+    const node2 = document.createTextNode('Enlace a...')
+    enlace.appendChild(node2);
+    enlace.setAttribute('href', 'https://www.ieszaidinvergeles.org')
+    
+    ptarea.appendChild(enlace);
+
+    const imagen = document.createElement('img');
+    tareas.appendChild(imagen);
+    imagen.setAttribute('src', 'https://eldiariony.com/wp-content/uploads/sites/2/2018/06/pedro-sanchez-gettyimages-963995924.jpg?quality=80&strip=all')
+    */
 }
