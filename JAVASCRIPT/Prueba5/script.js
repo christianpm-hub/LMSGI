a6eed5e3785345fa8247806fa5a537caf06bbf76
christianpm-hub <cpermon610@g.educaand.es>
2026-02-27 11:11:21 +0100

    Terminado con intentos

--- JAVASCRIPT/Prueba5/script.js
+++ JAVASCRIPT/Prueba5/script.js
@@ -1,16 +1,19 @@
 const numero = Math.floor(Math.random()*10)+1;
 const button = document.getElementById('probar');
 const input = document.getElementById('intento');
-
+let intentos = 0;
 button.addEventListener('click', AdivinarNumero);
 
 function AdivinarNumero(){
     const intento = Number(input.value);
     if(intento > numero){
-        alert('El número es menor');
+        intentos++;
+        alert('El número es menor. Llevas ' + intentos + ' intentos.');
     } else if (intento < numero){
-        alert('El número es mayor');
+        intentos++;
+        alert('El número es mayor. Llevas ' + intentos + ' intentos.');
     } else if(intento == numero){
-        alert('El número es correcto')
+        intentos++;
+        alert('El número es correcto. Adivinado en ' + intentos + ' intentos.')
     }
 }
