b2d77207e124bdc35c946687b3055be6e6a64703
christianpm-hub <cpermon610@g.educaand.es>
2026-03-04 14:08:04 +0100

    Ejercicio con una funcion

--- JAVASCRIPT/Prueba6/script.js
+++ JAVASCRIPT/Prueba6/script.js
@@ -4,27 +4,14 @@ const button1 = document.getElementById('boton1');
 const button2 = document.getElementById('boton2');
 const button3 = document.getElementById('boton3');
 
-button1.addEventListener('click', fboton1);
-button2.addEventListener('click', fboton2);
-button3.addEventListener('click', fboton3);
+button1.addEventListener('click', function(){fboton(0)});
+button2.addEventListener('click', function(){fboton(1)});
+button3.addEventListener('click', function(){fboton(2)});
 
-function fboton1(){
+function fboton(index){
     for (let i = 0; i < carrusel.length; i++){
         carrusel[i].style.display = 'none'; 
     }
-    carrusel[0].style.display = 'block';
+    carrusel[index].style.display = 'block';
 }
 
-function fboton2(){
-    for (let i = 0; i < carrusel.length; i++){
-        carrusel[i].style.display = 'none'; 
-    }
-    carrusel[1].style.display = 'block';
-}
-
-function fboton3(){
-    for (let i = 0; i < carrusel.length; i++){
-        carrusel[i].style.display = 'none'; 
-    }
-    carrusel[2].style.display = 'block';
-}
