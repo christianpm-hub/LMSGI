a5caf3efe6b5ddb4b54373413e1b4e322d5c8708
christianpm-hub <cpermon610@g.educaand.es>
2026-03-13 10:39:22 +0100

    Ejercicio3 Terminado

--- JAVASCRIPT/Ejercicios DOM/Ejercicio3/script.js
+++ JAVASCRIPT/Ejercicios DOM/Ejercicio3/script.js
@@ -2,3 +2,11 @@ const p = document.getElementById('mensaje');
 const button = document.getElementById('toggle');
 
 button.addEventListener('click', alternar);
+
+function alternar(){
+    if(p.style.display == 'none'){
+        p.style.display = 'block';
+    } else{
+        p.style.display = 'none';
+    }
+}
